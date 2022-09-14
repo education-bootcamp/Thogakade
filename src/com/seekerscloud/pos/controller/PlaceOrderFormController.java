e9eb2d03a18b3cd56905499c3e58d2aeb29cdd73
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-14 21:32:01 +0530

    3

--- src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
+++ src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
@@ -2,6 +2,7 @@ package com.seekerscloud.pos.controller;
 
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.modal.Customer;
+import com.seekerscloud.pos.modal.Item;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -11,10 +12,41 @@ import java.util.Date;
 public class PlaceOrderFormController {
     public TextField txtDate;
     public ComboBox<String> cmbCustomerIds;
+    public ComboBox<String> cmbItemCodes;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
 
     public void initialize(){
         setDateAndOrderId();
         loadAllCustomerIds();
+        loadAllItemCodes();
+
+        cmbCustomerIds.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if(newValue!=null){
+                        setCustomerDetails();
+                    }
+        });
+
+    }
+
+    private void setCustomerDetails() {
+        for (Customer c:Database.customerTable
+             ) {
+            if (c.getId().equals(cmbCustomerIds.getValue())){
+                txtName.setText(c.getName());
+                txtAddress.setText(c.getAddress());
+                txtSalary.setText(String.valueOf(c.getSalary()));
+            }
+        }
+    }
+
+    private void loadAllItemCodes() {
+        for(Item i : Database.itemTable){
+            cmbItemCodes.getItems().add(i.getCode());
+        }
     }
 
     private void loadAllCustomerIds() {
