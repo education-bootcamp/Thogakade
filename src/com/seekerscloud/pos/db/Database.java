23ebf48c446dc4b9cb3f8dea657ee41d8765075f
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 21:27:42 -0700

    5

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -1,6 +1,9 @@
 package com.seekerscloud.pos.db;
 
+import com.seekerscloud.pos.modal.Customer;
+
 import java.util.ArrayList;
+
 
 public class Database {
     public static ArrayList<Customer> customerTable
