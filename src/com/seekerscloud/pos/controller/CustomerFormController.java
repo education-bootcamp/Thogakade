70b77521b0624a6ca947643b3fdc977e56d77479
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 21:38:51 -0700

    6

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -1,6 +1,9 @@
 package com.seekerscloud.pos.controller;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.modal.Customer;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class CustomerFormController {
@@ -10,5 +13,15 @@ public class CustomerFormController {
     public TextField txtSalary;
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1= new Customer(txtId.getText(),
+                txtName.getText(),txtAddress.getText(),
+                Double.parseDouble(txtSalary.getText()));
+
+        boolean isSaved = Database.customerTable.add(c1);
+        if (isSaved){
+            new Alert(Alert.AlertType.INFORMATION, "Customer Saved!").show();
+        }else{
+            new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+        }
     }
 }
