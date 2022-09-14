231f514801770a742672ed9718e0852ada3c7da8
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 21:19:21 -0700

    4

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -1,4 +1,14 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
---
+++ src/com/seekerscloud/pos/db/Database.java
@@ -0,0 +1,8 @@
+package com.seekerscloud.pos.db;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Customer> customerTable
+            = new ArrayList<Customer>();
+}
