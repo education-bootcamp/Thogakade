08287b581ff53da8e87d790e760fead226445bce
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 20:20:32 -0700

    Initial commit

---
+++ src/com/seekerscloud/pos/controller/DashboardFormController.java
@@ -0,0 +1,21 @@
+package com.seekerscloud.pos.controller;
+
+import javafx.event.ActionEvent;
+
+public class DashboardFormController {
+    public void openCustomerFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openItemFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openPlaceOrderFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openOrderDetailsFormOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUi(String ui){
+        //----------
+    }
+}
---
+++ src/com/seekerscloud/pos/controller/OrderDetailsFormController.java
@@ -0,0 +1,4 @@
+package com.seekerscloud.pos.controller;
+
+public class OrderDetailsFormController {
+}
