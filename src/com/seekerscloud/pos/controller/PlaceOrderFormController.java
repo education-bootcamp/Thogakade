586c10f151d9a156d4b27838efe3e31810527fba
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-14 21:18:10 +0530

    2

--- src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
+++ src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
@@ -1,5 +1,8 @@
 package com.seekerscloud.pos.controller;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.modal.Customer;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.text.SimpleDateFormat;
@@ -7,8 +10,21 @@ import java.util.Date;
 
 public class PlaceOrderFormController {
     public TextField txtDate;
+    public ComboBox<String> cmbCustomerIds;
 
     public void initialize(){
+        setDateAndOrderId();
+        loadAllCustomerIds();
+    }
+
+    private void loadAllCustomerIds() {
+        for(Customer c: Database.customerTable){
+            cmbCustomerIds.getItems().add(c.getId());
+        }
+    }
+
+    private void setDateAndOrderId() {
+        // set Date
         /*Date date = new Date();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         String d =df.format(date);
