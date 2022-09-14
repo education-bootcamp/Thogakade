7af7267b2d46e96c31b7f25640398fdb87ec5bfb
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 21:50:42 -0700

    7

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -2,15 +2,26 @@ package com.seekerscloud.pos.controller;
 
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.modal.Customer;
+import com.seekerscloud.pos.view.tm.CustomerTm;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class CustomerFormController {
     public TextField txtId;
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+
+    public void initialize(){
+        searchCustomers();
+    }
+    private void searchCustomers(){
+        ObservableList<CustomerTm>
+    }
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
         Customer c1= new Customer(txtId.getText(),
---
+++ src/com/seekerscloud/pos/view/tm/CustomerTm.java
@@ -0,0 +1,62 @@
+package com.seekerscloud.pos.view.tm;
+
+import javafx.scene.control.Button;
+
+public class CustomerTm {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+    private Button btn;
+
+    public CustomerTm() {
+    }
+
+    public CustomerTm(String id, String name, String address, double salary, Button btn) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+        this.btn = btn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Button getBtn() {
+        return btn;
+    }
+
+    public void setBtn(Button btn) {
+        this.btn = btn;
+    }
+}
