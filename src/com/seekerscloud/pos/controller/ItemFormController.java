1386d8f2814aaf20ca3390b9486302e93bf7321f
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-14 00:10:14 -0700

    16

--- src/com/seekerscloud/pos/controller/ItemFormController.java
+++ src/com/seekerscloud/pos/controller/ItemFormController.java
@@ -1,12 +1,19 @@
 package com.seekerscloud.pos.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.modal.Customer;
+import com.seekerscloud.pos.modal.Item;
+import com.seekerscloud.pos.view.tm.CustomerTm;
 import com.seekerscloud.pos.view.tm.ItemTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.Optional;
 
 public class ItemFormController {
     public AnchorPane itemFormContext;
@@ -23,9 +30,75 @@ public class ItemFormController {
     public TableColumn colQtyOnHand;
     public TableColumn colOption;
 
+
+    private String searchText="";
+
+    public void initialize() {
+        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colQtyOnHand.setCellValueFactory(new PropertyValueFactory<>("qtyOnHand"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
+        searchItems(searchText);
+        tblItem.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (null!=newValue){// newValue!=null
+                        setData(newValue);
+                    }
+                });
+        txtSearch.textProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    searchText=newValue;
+                    searchItems(searchText);
+                });
+
+    }
+
+    private void setData(ItemTm tm) {
+        txtCode.setText(tm.getCode());
+        txtDescription.setText(tm.getDescription());
+        txtUnitPrice.setText(String.valueOf(tm.getUnitPrice()));
+        txtQtyOnHand.setText(String.valueOf(tm.getQtyOnHand()));
+        btnSaveItem.setText("Update Item");
+    }
+
+    private void searchItems(String text) {
+        ObservableList<ItemTm> tmList = FXCollections.observableArrayList();
+        for (Item i : Database.itemTable
+        ) {
+            if(i.getDescription().contains(text)){
+                Button btn = new Button("Delete");
+                ItemTm tm = new ItemTm(i.getCode(),i.getDescription(),i.getUnitPrice(),i.getQtyOnHand(), btn);
+                tmList.add(tm);
+
+                btn.setOnAction(event -> {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                            "are you sure whether do you want to delete this Item?",
+                            ButtonType.YES, ButtonType.NO);
+                    Optional<ButtonType> buttonType = alert.showAndWait();
+                    if (buttonType.get() == ButtonType.YES) {
+                        boolean isDeleted = Database.itemTable.remove(i);
+                        if (isDeleted) {
+                            searchItems(searchText);
+                            new Alert(Alert.AlertType.INFORMATION, "Item Deleted!").show();
+                        } else {
+                            new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+                        }
+                    }
+                });
+            }
+        }
+        tblItem.setItems(tmList);
+    }
+
     public void backToHomeOnAction(ActionEvent actionEvent) {
     }
 
     public void newItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveItemOnAction(ActionEvent actionEvent) {
     }
 }
--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -1,6 +1,7 @@
 package com.seekerscloud.pos.db;
 
 import com.seekerscloud.pos.modal.Customer;
+import com.seekerscloud.pos.modal.Item;
 
 import java.util.ArrayList;
 
@@ -8,4 +9,6 @@ import java.util.ArrayList;
 public class Database {
     public static ArrayList<Customer> customerTable
             = new ArrayList<Customer>();
+    public static ArrayList<Item> itemTable
+            = new ArrayList<Item>();
 }
