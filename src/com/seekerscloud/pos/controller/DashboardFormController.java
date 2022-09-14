75f03fe977989205ae9e29b07449466c0f425ab7
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-13 21:01:09 -0700

    2

--- src/com/seekerscloud/pos/controller/DashboardFormController.java
+++ src/com/seekerscloud/pos/controller/DashboardFormController.java
@@ -1,8 +1,35 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashboardFormController {
+    public AnchorPane dashboardContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+    private void setDateAndTime(){
+        // setTime
+        Timeline time = new Timeline(
+                new KeyFrame(Duration.ZERO, e->{
+                    DateTimeFormatter formatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    lblTime.setText(LocalDateTime.now().format(formatter));
+                }), new KeyFrame(Duration.seconds(1)));
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+    }
+
     public void openCustomerFormOnAction(ActionEvent actionEvent) {
     }
 
@@ -16,6 +43,6 @@ public class DashboardFormController {
     }
 
     private void setUi(String ui){
-        //----------
+
     }
 }
