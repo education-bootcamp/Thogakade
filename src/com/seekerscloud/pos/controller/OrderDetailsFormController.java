da4af3d4d3b05b5dd785f794011bfa0206bb59d3
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-15 20:39:38 +0530

    17

--- src/com/seekerscloud/pos/controller/OrderDetailsFormController.java
+++ src/com/seekerscloud/pos/controller/OrderDetailsFormController.java
@@ -1,4 +1,19 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class OrderDetailsFormController {
+    public AnchorPane orderDetailsContext;
+    public TableView tblOrders;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colDate;
+    public TableColumn colTotal;
+    public TableColumn colOption;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
 }
