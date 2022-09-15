3e5b8f884726b6ef07925263ef999d4b09889724
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-15 23:54:17 +0530

    31

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -68,15 +68,17 @@ public class CustomerFormController {
     }
 
     private void searchCustomers(String text) {
-
+        String searchText="%"+text+"%";
         try {
 
             ObservableList<CustomerTm> tmList = FXCollections.observableArrayList();
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade",
                     "root", "1234");
-            String sql = "SELECT * FROM Customer";
+            String sql = "SELECT * FROM Customer WHERE name LIKE ? || address LIKE ?";
             PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1,searchText);
+            statement.setString(2,searchText);
             ResultSet set = statement.executeQuery();
 
             while (set.next()){
