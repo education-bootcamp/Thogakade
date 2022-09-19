05d634dac68911b9dd335c71c0efa056d9b4c68b
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-19 22:38:44 +0530

    43

--- src/com/seekerscloud/pos/db/DBConnection.java
+++ src/com/seekerscloud/pos/db/DBConnection.java
@@ -1,6 +1,8 @@
 package com.seekerscloud.pos.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     // 1 rule
@@ -8,11 +10,18 @@ public class DBConnection {
     private Connection connection;
 
     // 2 rule
-    private DBConnection(){}
-
+    private DBConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection= DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/Thogakade",
+                "root","1234");
+    }
     // 3 rule
-    public static DBConnection getInstance(){
-        //
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection==null){
+            dbConnection= new DBConnection();
+        }
+        return dbConnection;
     }
 
     public Connection getConnection(){
