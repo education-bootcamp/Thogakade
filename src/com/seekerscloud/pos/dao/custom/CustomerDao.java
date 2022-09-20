b7b1825c7dd30d3de89e2fbd5a9eef33460a4c20
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-20 21:10:36 +0530

    55

---
+++ src/com/seekerscloud/pos/dao/custom/CustomerDao.java
@@ -0,0 +1,7 @@
+package com.seekerscloud.pos.dao.custom;
+
+import com.seekerscloud.pos.dao.CrudDao;
+import com.seekerscloud.pos.entity.Customer;
+
+public interface CustomerDao extends CrudDao<Customer,String> {
+}
---
+++ src/com/seekerscloud/pos/dao/custom/ItemDao.java
@@ -0,0 +1,7 @@
+package com.seekerscloud.pos.dao.custom;
+
+import com.seekerscloud.pos.dao.CrudDao;
+import com.seekerscloud.pos.entity.Item;
+
+public interface ItemDao extends CrudDao<Item, String> {
+}
