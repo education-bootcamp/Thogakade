15c7b94f35d8563d4e6892b6fe888376f8ed32af
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-20 23:02:33 +0530

    63

--- src/com/seekerscloud/pos/bo/custom/CustomerBo.java
+++ src/com/seekerscloud/pos/bo/custom/CustomerBo.java
@@ -1,5 +1,14 @@
 package com.seekerscloud.pos.bo.custom;
 
+import com.seekerscloud.pos.dto.CustomerDto;
+
+import java.util.ArrayList;
+
 public interface CustomerBo {
+
+    public boolean saveCustomer(CustomerDto dto);
+    public boolean updateCustomer(CustomerDto dto);
+    public boolean deleteCustomer(String id);
+    public ArrayList<CustomerDto> searchCustomers(String searchText);
 
 }
--- src/com/seekerscloud/pos/bo/custom/ItemBo.java
+++ src/com/seekerscloud/pos/bo/custom/ItemBo.java
@@ -1,4 +1,14 @@
 package com.seekerscloud.pos.bo.custom;
 
+import com.seekerscloud.pos.dto.CustomerDto;
+import com.seekerscloud.pos.dto.ItemDto;
+
+import java.util.ArrayList;
+
 public interface ItemBo {
+    public boolean saveItem(ItemDto dto);
+    public boolean updateItem(ItemDto dto);
+    public boolean deleteItem(String id);
+    public ArrayList<ItemDto> searchItems(String searchText);
+
 }
