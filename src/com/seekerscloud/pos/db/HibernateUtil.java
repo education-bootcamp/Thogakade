08831df94bfb7c2b7689c79138e9d522972bc1e7
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-30 23:52:18 +0530

    70

---
+++ src/com/seekerscloud/pos/db/HibernateUtil.java
@@ -0,0 +1,38 @@
+package com.seekerscloud.pos.db;
+
+import com.seekerscloud.pos.entity.Customer;
+import com.seekerscloud.pos.entity.Item;
+import com.seekerscloud.pos.entity.Order;
+import com.seekerscloud.pos.entity.OrderDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory= buildSessionFactory();
+    private HibernateUtil(){}
+    private static SessionFactory buildSessionFactory(){
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("../hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Order.class)
+                .addAnnotatedClass(OrderDetails.class)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder()
+                .build();
+    }
+    public static Session createSession(){
+        return sessionFactory.openSession();
+    }
+}
